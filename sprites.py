280ddc3584ca2e96b5736b3b5a8ffd4a7210f17a
MiguelMFR <guelferreirar@gmail.com>
2026-05-31 19:38:15 -0300

    feat: update player sprites

--- sprites.py
+++ sprites.py
@@ -38,10 +38,10 @@ background_game = sprite.Sprite("images/backgrounds/fase1.png")
 background_game.set_position(0, 0)
 
 # PLAYER
-player_up = sprite.Sprite("images/player/jogador-up.png", 4)
-player_right = sprite.Sprite("images/player/jogador-right.png", 4)
-player_left = sprite.Sprite("images/player/jogador-left.png", 4)
-player_down = sprite.Sprite("images/player/jogador-down.png", 4)
+player_up = sprite.Sprite("images/player/player-up.png", 4)
+player_right = sprite.Sprite("images/player/player-right.png", 4)
+player_left = sprite.Sprite("images/player/player-left.png", 4)
+player_down = sprite.Sprite("images/player/player-down.png", 4)
 
 player_up.set_total_duration(333)
 player_right.set_total_duration(333)
@@ -58,3 +58,10 @@ player_up.pause()
 player_right.pause()
 player_left.pause()
 player_down.pause()
+
+# Volume icon
+# VOLUME 
+volume_on = sprite.Sprite("images/icon-sound/sound-on.png", 1)
+volume_on.set_position(config.janela.largura - volume_on.width - 10, 10)
+volume_off = sprite.Sprite("images/icon-sound/sound-off.png", 1)
+volume_off.set_position(config.janela.largura - volume_off.width - 10, 10)
