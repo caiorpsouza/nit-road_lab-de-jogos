e6c343eb50e6c1f0bb0783d9fb3589d58aa1ed3e
Caio <caioc3po@gmail.com>
2026-05-30 22:00:35 -0300

    feat: nova entidade CarSpawner

---
+++ entities/Carro.py
@@ -0,0 +1,16 @@
+
+class Carro:
+    def __init__(self, sprite, x, y, speed):
+        self.sprite = sprite
+        self.x = x
+        self.y = y
+        self.speed= speed
+    
+    def set_position(self, x, y):
+        self.sprite.set_position(x, y)
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        self.sprite.update()
+        self.sprite.draw()
