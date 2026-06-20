b4e274f68a987d1855d39fbb48fe95b441fc0f6c
MiguelMFR <guelferreirar@gmail.com>
2026-06-20 08:15:07 -0300

    feat: initialize window with fullscreen mode and add logo

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
 from PPlay import *
+import pygame  # type: ignore[import-not-found]
+from assets import resource_path
 
+pygame.init()
 janela = window.Window(1400, 960, "Nit Road")
+
+info_tela = pygame.display.Info()
+janela.real_screen = pygame.display.set_mode(
+	(info_tela.current_w, info_tela.current_h),
+	pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE,
+)
 
 keyboard = janela.keyboard
 mouse = janela.mouse
+
+icone = pygame.image.load(resource_path("images/logo/logo.png"))
+pygame.display.set_icon(icone)
 
 
 Tela = 'Menu'
